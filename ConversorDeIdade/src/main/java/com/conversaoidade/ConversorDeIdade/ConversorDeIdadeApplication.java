bdf9fb4abec0a3607b41141307632cabc1a72c75
otonielalmeida <otonieledward@gmail.com>
2020-06-24 15:42:32 -0300

    Exercicio Lombok2

---
+++ ConversorDeIdade/src/main/java/com/conversaoidade/ConversorDeIdade/ConversorDeIdadeApplication.java
@@ -0,0 +1,13 @@
+package com.conversaoidade.ConversorDeIdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorDeIdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorDeIdadeApplication.class, args);
+	}
+
+}
---
+++ ConversorDeIdade/src/test/java/com/conversaoidade/ConversorDeIdade/ConversorDeIdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversaoidade.ConversorDeIdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorDeIdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
